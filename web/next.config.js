7bff3db3fe95f5c83479affc306827914ccc7aa2
Bajeeraok <84369134+Bajeeraok@users.noreply.github.com>
2025-12-04 00:16:24 -0500

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    domains: ['images.unsplash.com','res.cloudinary.com'],
   },
 };
 
