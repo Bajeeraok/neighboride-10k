72f9a6d14628d7f56f4537c3c7cd06a12807bb9a
Bajeeraok <84369134+Bajeeraok@users.noreply.github.com>
2025-12-03 23:39:10 -0500

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
+
+module.exports = nextConfig;
